4c6f3b90f7e4994b376425bb102843ab1da5b497
my powerful PC <peijin.he@qq.com>
2021-06-22 02:26:32 +1200

    url fix

--- src/Api/index.jsx
+++ src/Api/index.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import {baseurl} from "../Config";
 
-export const preSubmit = (values, controllerName) => {
-    return axios.post(baseurl+`User/${controllerName}`, values)
+export const preSubmit = (values) => {
+    debugger
+    console.log(baseurl+`Survey/AcquireSurveyP1`);
+    return axios.post(baseurl+`Survey/AcquireSurveyP1`, values)
 }
