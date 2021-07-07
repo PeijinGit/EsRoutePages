2321a17bd3109a813f0f34a6525b3d0ac514a113
my powerful PC <peijin.he@qq.com>
2021-07-08 01:09:16 +1200

    extract update

--- src/containers/Extract/extract.js
+++ src/containers/Extract/extract.js
@@ -1,12 +1,52 @@
 import React from 'react'
 import { Button } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
+import './extract.scss'
 
 export default function extract() {
+
+    const tsetFunc = () => {
+        console.log("stestst");
+    }
     return (
         <div className="outline">
             <div className="MainBody">
-                <Button type="primary" icon={<DownloadOutlined />} size={'large'}>
+                <Button
+                    type="primary"
+                    icon={<DownloadOutlined />}
+                    size={'large'}
+                    onClick={tsetFunc}
+                >
+                    Download
+                </Button>
+            </div>
+            <div className="MainBody">
+                <Button
+                    type="primary"
+                    icon={<DownloadOutlined />}
+                    size={'large'}
+                    onClick={tsetFunc}
+                >
+                    Download
+                </Button>
+            </div>
+            <div className="MainBody">
+                <Button
+                    type="primary"
+                    icon={<DownloadOutlined />}
+                    size={'large'}
+                    onClick={tsetFunc}
+                >
+                    Download
+                </Button>
+            </div>
+            <div className="MainBody">
+                <Button
+                    type="primary"
+                    icon={<DownloadOutlined />}
+                    size={'large'}
+                    onClick={tsetFunc}
+                >
                     Download
                 </Button>
             </div>
